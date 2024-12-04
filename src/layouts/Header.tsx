19c791cd268f1c73081b8039afdeb512ab401a7c
Openstuck <ojokdibalas@gmail.com>
2024-12-05 02:06:27 +0700

    update

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
   return (
     <Navbar expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Greenfolio Bridge</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
