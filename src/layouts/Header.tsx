54a4915f1b399b7d1a6cce14e5b64d87cd500601
Openstuck <ojokdibalas@gmail.com>
2024-12-11 03:47:44 +0700

    update header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -10,10 +10,10 @@ export const Header = () => {
         <Navbar.Brand href="#home">Greenfolio Bridge</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          {/* <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
+          </Nav> */}
         </Navbar.Collapse>
         <appkit-button />
       </Container>
